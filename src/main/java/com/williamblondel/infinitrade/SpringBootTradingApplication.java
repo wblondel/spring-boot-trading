d0bf78f6418794799f10209fe17162d69a9ae2f0
William Gérald Blondel <contact@williamblondel.fr>
2024-06-05 09:19:31 +0800

    initial commit

---
+++ src/main/java/com/williamblondel/infinitrade/SpringBootTradingApplication.java
@@ -0,0 +1,13 @@
+package com.williamblondel.infinitrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/williamblondel/infinitrade/SpringBootTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamblondel.infinitrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
