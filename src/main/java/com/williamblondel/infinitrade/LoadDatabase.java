77763a9a72b7710b88ac6a6886bc346444e85b59
William Gérald Blondel <contact@williamblondel.fr>
2024-06-05 15:41:36 +0800

    (seeders) More data to make sure the relationships work

--- src/main/java/com/williamblondel/infinitrade/LoadDatabase.java
+++ src/main/java/com/williamblondel/infinitrade/LoadDatabase.java
@@ -39,16 +39,28 @@ public class LoadDatabase {
             Currency btcCurrency = new Currency("BTC", "Bitcoin", "Bitcoin is the world’s first cryptocurrency designed to operate decentralized over a blockchain. Unlike traditional currencies, it can be used as a store of value and for making digital payments without a central authority like a bank or a financial institution. The symbol BTC in the market represents Bitcoin.", "https://bitcoin.org");
             Currency ethCurrency = new Currency("ETH", "Ethereum", "Ethereum (ETH) is a decentralized, open-source blockchain system with smart contract functionality. In terms of market capitalization, it is one of the most prominent cryptocurrencies, second only to Bitcoin.", "https://ethereum.org");
             Currency usdtCurrency = new Currency("USDT", "Tether", "Tether (USDT) is globally the first and most widely used stablecoin in the crypto market. The USDT price is pegged to the US dollar at a 1 to 1 ratio and is backed by Tether's reserves.", "https://tether.to");
+            Currency usdcCurrency = new Currency("USDC", "USD Coin", "USD Coin (USDC) is the second most popularly used stablecoin pegged to the US dollar. It gives all the benefits of cryptocurrency without worrying about high volatility due to its peg and is issued by regulated financial institutions.", "https://www.circle.com");
 
             // Save currencies
-            log.info("Preloading {}", currencyRepository.saveAll(List.of(btcCurrency, ethCurrency, usdtCurrency)));
+            log.info("Preloading {}", currencyRepository.saveAll(List.of(btcCurrency, ethCurrency, usdtCurrency, usdcCurrency)));
 
             // Create pairs
             Pair ethUsdtPair = new Pair(ethCurrency, usdtCurrency);
             Pair btcUsdtPair = new Pair(btcCurrency, usdtCurrency);
+            Pair ethBtcPair = new Pair(ethCurrency, btcCurrency);
+            Pair usdcUsdtPair = new Pair(usdcCurrency, usdtCurrency);
+            Pair btcUsdcPair = new Pair(btcCurrency, usdcCurrency);
+            Pair ethUsdcPair = new Pair(ethCurrency, usdcCurrency);
 
             // Save pairs
-            log.info("Preloading {}", pairRepository.saveAll(List.of(ethUsdtPair, btcUsdtPair)));
+            log.info("Preloading {}", pairRepository.saveAll(List.of(
+                    ethUsdtPair,
+                    btcUsdtPair,
+                    ethBtcPair,
+                    usdcUsdtPair,
+                    btcUsdcPair,
+                    ethUsdcPair
+            )));
 
             // Create wallet
             Wallet johnDoeUsdtWallet = new Wallet(johnDoeUser, usdtCurrency, 50000.0);
